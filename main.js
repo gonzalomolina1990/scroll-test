b656116bb8797df444e02fa4da804e210f2ac4a1
gonzalo.molina@wundermanthompson.com <gonzalo.molina@wundermanthompson.com>
2023-07-06 18:35:33 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+
+
+var sections = [];
+var currentScroll = -1;
+
+
+
+document.querySelectorAll("section").forEach(element => {
+    sections.push(element);
+});
+
+
+
+
+document.querySelector(".scroller-up").addEventListener("click", ()=>{
+    if (currentScroll > -1) {
+        currentScroll--;
+        console.log(1, currentScroll);
+    } else{
+        currentScroll = sections.length-2;
+        console.log(2, currentScroll);
+
+    }
+    sections[currentScroll+1].scrollIntoView({ behavior: 'smooth' });
+
+});
+
+document.querySelector(".scroller-down").addEventListener("click", ()=>{
+    if (currentScroll < sections.length-2) {
+        currentScroll++;
+    } else{
+        currentScroll = -1;
+    }
+    sections[currentScroll+1].scrollIntoView({ behavior: 'smooth' });
+
+});
+
